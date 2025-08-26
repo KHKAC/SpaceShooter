a556a2893f4348323ed8a0982f7a34d2375cd920
KHKAC <rudgnsrb888@naver.com>
2025-08-26 14:38:53 +0900

    250826 절대강좌 Player 이동

--- Assets/02.Script/PlayerCtrl.cs
+++ Assets/02.Script/PlayerCtrl.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class PlayerCtrl : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // component cash
+    Transform tr;
     void Start()
     {
-        
+        // GetComponent
+        // tr = this.gameObject.GetComponent<Transform>();
+        // GetComponent("Transform") as Transform;
+        // tr = (Transform)GetComponent((typeof(Transform)))
+        // 실제로 사용하는 것.
+        tr = GetComponent<Transform>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        Debug.Log($"h = {h}");
+        Debug.Log($"v = {v}");
+        // Transform.Position과 normalized Vector의 표현 방법 차이
+        // transform.position += new Vector3(0, 0, 1); // Transform.Position
+        tr.position += Vector3.forward * 1; // normalized Vector
     }
+    
+
 }
