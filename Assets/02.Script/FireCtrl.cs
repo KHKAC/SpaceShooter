32b27de195795352695ebe308772d6f14eae85d6
KHKAC <rudgnsrb888@naver.com>
2025-09-12 13:51:19 +0900

    250912 절대강좌 총소리 추가

--- Assets/02.Script/FireCtrl.cs
+++ Assets/02.Script/FireCtrl.cs
@@ -2,14 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// 반드시 필요한 컴포넌트를 명시하는 Attribute
+[RequireComponent(typeof(AudioSource))]
 public class FireCtrl : MonoBehaviour
 {
     public GameObject bullet;
     public Transform firePos;
+    public AudioClip fireSfx;
+
+    AudioSource fireAudio;
 
     void Start()
     {
-
+        fireAudio = GetComponent<AudioSource>();
     }
 
     void Update()
@@ -23,5 +28,6 @@ public class FireCtrl : MonoBehaviour
     void Fire()
     {
         Instantiate(bullet, firePos.position, firePos.rotation);
+        fireAudio.PlayOneShot(fireSfx, 1.0f);
     }
 }
